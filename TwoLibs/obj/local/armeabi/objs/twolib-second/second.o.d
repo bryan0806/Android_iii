3c564e2d89fb672afee61c31f679e70a61aa9572
Bryan@iii <chunwei.liu@gmail.com>
2014-07-31 17:43:48 +0800

    第二個 利用JNI 來CALL C函數的範例 此範例在利用兩個C函數 計算兩個數字的SUM

---
+++ TwoLibs/obj/local/armeabi/objs/twolib-second/second.o.d
@@ -0,0 +1,15 @@
+obj/local/armeabi/objs/twolib-second/second.o: jni/second.c jni/first.h \
+ /root/android-ndk-r9d/platforms/android-3/arch-arm/usr/include/jni.h \
+ /root/android-ndk-r9d/platforms/android-3/arch-arm/usr/include/sys/cdefs.h \
+ /root/android-ndk-r9d/platforms/android-3/arch-arm/usr/include/sys/cdefs_elf.h \
+ /root/android-ndk-r9d/platforms/android-3/arch-arm/usr/include/android/api-level.h
+
+jni/first.h:
+
+/root/android-ndk-r9d/platforms/android-3/arch-arm/usr/include/jni.h:
+
+/root/android-ndk-r9d/platforms/android-3/arch-arm/usr/include/sys/cdefs.h:
+
+/root/android-ndk-r9d/platforms/android-3/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/root/android-ndk-r9d/platforms/android-3/arch-arm/usr/include/android/api-level.h:
